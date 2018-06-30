daff6839ec658d62ac8dcd4ba9f8613d8d77b667
Khabibullin Timur <timur.khab@gmail.com>
2018-06-30 14:21:16 +0300

    Fixed wrong OVerride for new versions of RN

--- android/src/main/java/com/github/xfumihiro/react_native_image_to_base64/ImageToBase64Package.java
+++ android/src/main/java/com/github/xfumihiro/react_native_image_to_base64/ImageToBase64Package.java
@@ -22,7 +22,7 @@ public class ImageToBase64Package implements ReactPackage {
     return modules;
   }
 
-  @Override
+  //@Override
   public List<Class<? extends JavaScriptModule>> createJSModules() {
     return Collections.emptyList();
   }
